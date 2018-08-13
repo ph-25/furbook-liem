0825b74e1fd469a1ff6579b3298e93befdbe9f5f
=HuynhLiem <=huynhliemqt21@gmail.com>
2018-08-13 18:48:17 +0700

    them route

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,35 @@ Route::get('/', function(){
 	return redirect('/cats');
 });
 
-
-Route::get('/cats', ['uses' => 'CatController@index', 'as' => 'cat.index']);
-	//echo "danh sach cat";exit;
-	//$cats = '<h1>List all cats</h1>';
+Route::group(['middleware' => 'auth'], function(){
+	Route::get('/cats', ['uses' => 'CatController@index', 'as' => 'cat.index']);
 	
+	//hiển thị thông tin của một con mèo
+	Route::get('cats/breeds/{name}', function($name){
+		$breed = Breed::with('cats')
+		->where('name', $name)
+		->first();
+		return view('cats/index')
+		->with('breed', $breed)
+		->with('cats', $breed->cats);
+	});
+	// hiển thị chi tiết một con mèo qua id
+	Route::get('/cats/{cat}', ['uses' => 'CatController@show', 'as' => 'cat.show'])->where('cat', '[0-9]+');
 
+	//create new cat
+	Route::get('cats/create', ['uses' => 'CatController@create', 'as' => 'cat.create']);
 
+	//insert new cat
+	Route::post('/cats', ['uses' => 'CatController@store', 'as' => 'cat.store']);
+
+	//edit cat
+	Route::get('/cats/{cat}/edit', ['uses' => 'CatController@edit', 'as' => 'cat.edit']);
+
+	Route::put('/cats/{cat}', ['uses' => 'CatController@update', 'as' => 'cat.update']);
+
+	//delete cat
+	Route::delete('cats/{id}', ['uses' => 'CatController@destroy', 'as' => 'cat.destroy']);
+});
 
 /*//hiển thị chi tiết một con mèo thông qua id
 Route::get('/cats/{id}', function($id) {
@@ -39,33 +61,5 @@ Route::get('/cats/{id}', function($id) {
 	return view('cats.show')->with('cat', $cat);
 });*/
 
-//hiển thị thông tin của một con mèo
-Route::get('cats/breeds/{name}', function($name){
-	$breed = Breed::with('cats')
-	->where('name', $name)
-	->first();
-	return view('cats/index')
-	->with('breed', $breed)
-	->with('cats', $breed->cats);
-});
-
-// hiển thị chi tiết một con mèo qua id
-Route::get('/cats/{cat}', ['uses' => 'CatController@show', 'as' => 'cat.show'])->where('cat', '[0-9]+');
-
-//create new cat
-Route::get('cats/create', ['uses' => 'CatController@create', 'as' => 'cat.create']);
-
-//insert new cat
-Route::post('/cats', ['uses' => 'CatController@store', 'as' => 'cat.store']);
-
-
-//edit cat
-Route::get('/cats/{id}/edit', ['uses' => 'CatController@edit', 'as' => 'cat.edit']);
-
-Route::put('/cats/{id}', ['uses' => 'CatController@update', 'as' => 'cat.update']);
-
-//delete cat
-Route::delete('cats/{id}', ['uses' => 'CatController@destroy', 'as' => 'cat.destroy']);
-
-
-
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
